1d2eb33dd7bb00d087a48211c9d68900de925df2
Vincensius Augustino <vincensius.august@gmail.com>
2018-12-20 22:17:27 +0700

    a

--- FlaskSite/Config.py
+++ FlaskSite/Config.py
@@ -3,13 +3,6 @@ import os
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///static/Database/Site.db'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
     SEND_FILE_MAX_AGE_DEFAULT = 0
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') # Bisa di ganti langsung dengan alamat email
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') # Bisa di ganti langsung dengan password email
